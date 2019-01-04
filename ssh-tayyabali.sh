adb41555e29920630f3505fcb831580242e2e81d
Tayyabali <tayyabsayyad@gmail.com>
2019-01-04 20:11:17 +0530

    Shell Removed

--- ssh-tayyabali.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# This script does rsync with the tayyabali.in server 
-#rsync -a -v /home/tayyabali/websites/tayyabali.in/public/  tayyabali.in@ssh.stackcp.com:/home/sites/5a/b/b6fb4ddc49/public_html/
-
-
-rsync -a -v /home/tayyabali/Websites/tayyabali.in/public/  root@142.93.223.127:/var/www/tayyabali.in/public_html
-
